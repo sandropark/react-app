d46c436fa205e27d4a983a78ca36840094d6ac2b
sandropark <ticoba@naver.com>
2024-06-25 21:50:46 +0900

    effect - 특정 상태가 변경될 때만 실행되는 코드 지정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setCounter((prev) => prev + 1);
+  const onChange = (e) => setKeyword(e.target.value);
+  // 처음 랜더 될 때 한 번만 실행
+  useEffect(() => {
+    console.log("I run only once");
+  }, []);
+  // keyword가 변경될 때마다 실행
+  useEffect(() => {
+    console.log("I run whe 'keyword' changes");
+  }, [keyword]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text="continue" />
+      <input type="text" value={keyword} onChange={onChange} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
