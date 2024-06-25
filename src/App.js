5ffbb24e6e0adc9c56820c80c512393bceed9cae
sandropark <ticoba@naver.com>
2024-06-25 22:12:32 +0900

    effect : 컴포넌트가 사라질 때 실행되는 함수 설정

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setCounter((prev) => prev + 1);
-  const onChange = (e) => setKeyword(e.target.value);
-  // 처음 랜더 될 때 한 번만 실행
+function Hello() {
   useEffect(() => {
-    console.log("I run only once");
+    console.log("Hi.");
+    return () => console.log("Bye");
   }, []);
-  // keyword가 변경될 때마다 실행
-  useEffect(() => {
-    console.log("I run whe 'keyword' changes");
-  }, [keyword]);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input type="text" value={keyword} onChange={onChange} />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
